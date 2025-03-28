b0b0c3bc220127d5d3f5a67de1a7a100f6a4a507
Luis Vargas Rojas <luizvargaz@hotmail.com>
2025-03-28 19:01:37 -0400

    Data from GitHub

--- app.py
+++ app.py
@@ -231,5 +231,6 @@ def update_charts(selected_section, selected_question, selected_grade, selected_
 
 
 # # Run app
+server = app.server
 # if __name__ == '__main__':
 #     app.run(debug=False, host="0.0.0.0", port=8080)
